88ffb7e38bd13d0c64c1a01fef1f981c84d0fd96
manavkandoi <mkandoi2003@gmail.com>
2023-12-08 02:59:11 +0530

    changed server

--- server/app/index.ts
+++ server/app/index.ts
@@ -14,10 +14,13 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send({ msg: 'This has CORS enabled 🎈' });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
+  
     
 //app.use('/api/', helloRouter);
 app.use('/api/user', userRouter);
