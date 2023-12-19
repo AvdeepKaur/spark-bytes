7a93fb423af3324aa84293707720531dca1d3dee
manavkandoi <mkandoi2003@gmail.com>
2023-12-20 04:01:53 +0530

    minor

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ server.use(
     origin: 'https://spark-bytes-project-team-7-git-main-spark-team-7.vercel.app/',
   })
 );
-server.use(app);x
+server.use(app);
 app.listen(port, (err?: any) => {
   if (err) throw err;
   console.log('> Ready on https://cs392-team-7-e01a3988ee9c.herokuapp.com/');
