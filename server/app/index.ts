2f0ea45e4aec79fe5c101cb39ee711e849d49715
manavkandoi <mkandoi2003@gmail.com>
2023-12-20 04:38:52 +0530

    changed server

--- server/app/index.ts
+++ server/app/index.ts
@@ -11,7 +11,11 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://spark-bytes-project-team-7-eight.vercel.app/',
+  })
+);
 app.use(express.json());
 
 //app.use('/api/', helloRouter);
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,11 @@
+import express from 'express';
+import cors from 'cors';
 import app from './app/index.ts';
 const port = process.env.PORT || '5005';
+const server = express();
+server.use(cors());
+server.use(app);
 app.listen(port, (err?: any) => {
   if (err) throw err;
-  console.log('> Ready on http://localhost:5005');
+  console.log('> Ready on https://cs392-team-7-e01a3988ee9c.herokuapp.com/');
 });
