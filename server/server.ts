1ba331a995d32238cbecef7ce1458261f78e1876
manavkandoi <mkandoi2003@gmail.com>
2023-12-20 03:34:05 +0530

    server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,14 @@
+import express from 'express';
+import cors from 'cors';
 import app from './app/index.ts';
 const port = process.env.PORT || '5005';
+const server = express();
+server.use(
+  cors({
+    origin: 'https://spark-bytes-project-team-7-git-main-spark-team-7.vercel.app',
+  })
+);
+server.use(app);
 app.listen(port, (err?: any) => {
   if (err) throw err;
   console.log('> Ready on https://cs392-team-7-e01a3988ee9c.herokuapp.com/');
