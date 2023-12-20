48b2f8f7f567dfd1c7c8b67a937d40ae2f9fb0d3
KimDohyun <jdkim128@bu.edu>
2023-12-20 18:32:02 -0500

    new attempt at location

--- client/src/pages/events/[event_id].tsx
+++ client/src/pages/events/[event_id].tsx
@@ -68,8 +68,11 @@ const Events: React.FC = () => {
         if (record.field === 'location') {
           if (value && value.Address !== undefined && value.floor !== undefined && value.room !== undefined) {
             return `${value.Address}, Floor ${value.floor}, Room ${value.room}`;
-          } else {
+          } else if (value && typeof value === 'object') {
+            console.log("Value is an object:", value);
             return 'Not specified';
+          } else {
+            return value ? value.toString() : 'Not specified';
           }
         }
 
