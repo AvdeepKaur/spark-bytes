c27c19fc2114748d5dd8745adbc88ce050960ee3
manavkandoi <mkandoi2003@gmail.com>
2023-12-21 06:43:34 +0530

    param type

--- client/src/pages/events/[event_id].tsx
+++ client/src/pages/events/[event_id].tsx
@@ -86,7 +86,7 @@ const Events: React.FC = () => {
 
         if (record.field === "photos") {
           // Assuming 'value' is an array of objects with a 'photo' property
-          return value.map((photoObj, index: any) => (
+          return value.map((photoObj: any, index: any) => (
             <img
               key={index}
               src={`data:image/png;base64,8J-kng`} // Replace 'jpeg' with the correct format, if necessary
